7fbd56843a4f5e6db4a2eecc979df7459203c759
Kim Le <kle@abo.fi>
2018-03-04 12:51:17 +0200

    fixed .gitignore

---
+++ bfg/bfg.go
@@ -0,0 +1,56 @@
+package bfg
+
+import (
+	"math/rand"
+	"sort"
+	"time"
+)
+
+// The ingredients for a true exit fragger experience
+const (
+	Svamp     = "svamp"
+	Majs      = "majs"
+	Paprika   = "paprika"
+	Makaroner = "makaroner"
+	Hona      = "höna"
+	Kott      = "kött"
+	Vitsas    = "vit sås"
+)
+
+var ingredients []string
+var meal map[string]struct{}
+
+// Meal is the struct to be JSON encoded and sent to the client
+type Meal struct {
+	Ingredients []string `json:"ingredients"`
+}
+
+// SetUp sets up by populating the ingredients slice etc.
+func SetUp() {
+	rand.Seed(time.Now().UnixNano())
+	ingredients = append(ingredients, Svamp, Majs, Paprika, Makaroner, Hona, Kott, Vitsas)
+	meal = make(map[string]struct{})
+}
+
+// Generate returns a struct of type Meal, which contains a sorted slice.
+func Generate() Meal {
+	var dish Meal
+	if len(meal) >= 4 {
+		meal = make(map[string]struct{})
+	}
+	for {
+		if len(meal) >= 4 {
+			for ingr := range meal {
+				dish.Ingredients = append(dish.Ingredients, ingr)
+			}
+			break
+		}
+
+		ingredient := ingredients[rand.Intn(len(ingredients))]
+		if _, ok := meal[ingredient]; !ok {
+			meal[ingredient] = struct{}{}
+		}
+	}
+	sort.Strings(dish.Ingredients)
+	return dish
+}
