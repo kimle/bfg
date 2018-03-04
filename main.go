5ff71e67bc97383c78a3b23df0d183751f679924
Kim Le <kle@abo.fi>
2018-03-04 12:49:24 +0200

    added static files files and refactored

--- main.go
+++ main.go
@@ -1,66 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
-	"math/rand"
+	"mime"
 	"net/http"
-	"time"
+
+	"github.com/kimle/bfg/bfg"
+	"github.com/rs/cors"
 )
-
-// The core ingredients for an exit fragger meal
-const (
-	Svamp     = "svamp"
-	Majs      = "majs"
-	Paprika   = "paprika"
-	Makaroner = "makaroner"
-	Hona      = "höna"
-	Kott      = "kött"
-	Vitsas    = "vit sås"
-)
-
-var ingredients []string
-var meal map[string]struct{}
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-	ingredients = append(ingredients, Svamp, Majs, Paprika, Makaroner, Hona, Kott, Vitsas)
-	meal = make(map[string]struct{})
-}
 
 func index(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprint(w, "the complete dish: ")
-	if err != nil {
-		log.Fatal(err)
-	}
-	if len(meal) > 0 {
-		for key := range meal {
-			_, err := fmt.Fprintf(w, "%v ", key)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
+	http.ServeFile(w, r, "static/index.html")
 }
 
 func addIngredient(w http.ResponseWriter, r *http.Request) {
-	if len(meal) >= 3 {
-		meal = make(map[string]struct{})
-		return
+	w.Header().Set("Content-Type", "application/json")
+	meal := bfg.Generate()
+	err := json.NewEncoder(w).Encode(meal)
+	if err != nil {
+		log.Fatal(err)
 	}
-	for {
-		ingredient := ingredients[rand.Intn(len(ingredients))]
-		if _, ok := meal[ingredient]; !ok {
-			meal[ingredient] = struct{}{}
-			break
-		}
-	}
-	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
+	bfg.SetUp()
+	if err := mime.AddExtensionType(".js", "application/javascript; charset=utf-8"); err != nil {
+		log.Println(err)
+	}
+	if err := mime.AddExtensionType(".css", "text/css; charset=utf-8"); err != nil {
+		log.Println(err)
+	}
 	log.Println("listening on port 8080...")
-	http.HandleFunc("/", index)
-	http.HandleFunc("/add", addIngredient)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/add", addIngredient)
+	handler := cors.Default().Handler(mux)
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
