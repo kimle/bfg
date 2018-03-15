e2d795a3d92343060b0662ed1757ef9908bca54d
Kim Le <kle@abo.fi>
2018-03-15 22:22:05 +0200

    added meek card

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import Button from 'material-ui/Button';
 import Grid from 'material-ui/Grid';
+import Restaurant from 'material-ui-icons/Restaurant';
+
 import './App.css';
+import MeekCard from './MeekCard';
 
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      meal: []
+      hasGenerated: false,
+      meal: [],
+      days: null
     }
     this.getMeal = this.getMeal.bind(this);
+  }
+
+  componentWillMount() {
+    this.getMeekDays();
   }
 
   getMeal() {
@@ -22,6 +31,7 @@ class App extends Component {
     .then(data => {
       if (data) {
         this.setState({
+          hasGenerated: true,
           meal: data.ingredients
         })
       }
@@ -29,24 +39,45 @@ class App extends Component {
     .catch(error => console.log(error));
   }
 
+  getMeekDays() {
+    return fetch(window.location.origin + '/freemeek', {
+      method: 'GET',
+      mode: 'cors',
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data) {
+        this.setState({ days: data.days });
+      }
+    })
+    .catch(error => console.log(error));
+  }
+
   render() {
+    const hasGenerated = this.state.hasGenerated;
+    
     return (
       <div className="App">
         <h2>BFG</h2>
         <p>Click the generate button for a yummy dish!</p>
         <div className="App">
           <Grid item lg={12}>
+            { hasGenerated ?
             <div className="App">
               <h3>Your meal: </h3>
               {this.state.meal.map((value, index) => <strong key={index}><p>{value}</p></strong>)}
-            </div>
+            </div> : null }
           </Grid>
           <Grid item lg={12}>
             <div className="App">
-              <Button variant="raised" color="primary" onClick={this.getMeal}>Generate</Button>
+              <Button className="button" variant="raised" color="secondary" onClick={this.getMeal}>
+                Generate
+                <Restaurant className="rightIcon"></Restaurant>
+              </Button>
             </div>
           </Grid>
         </div>
+        <MeekCard days={this.state.days}/>
       </div>
     );
   }
--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"mime"
 	"net/http"
+	"time"
 
 	bfg "github.com/kimle/bfg/pkg/generator"
 	"github.com/rs/cors"
@@ -22,6 +24,21 @@ func getIngredients(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func getMeekTime(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	loc, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		loc = time.UTC
+		log.Printf("could not load tzdata: %v\n", err)
+	}
+	courtdate := time.Date(2017, time.November, 6, 0, 0, 0, 0, loc)
+	daysSince := math.Floor(time.Since(courtdate).Hours() / (time.Hour * 24).Hours())
+	err = json.NewEncoder(w).Encode(map[string]interface{}{"days": daysSince})
+	if err != nil {
+		log.Fatalf("app must have message about freeing meek mill: %v\n", err)
+	}
+}
+
 func main() {
 	bfg.SetUp()
 	if err := mime.AddExtensionType(".js", "application/javascript; charset=utf-8"); err != nil {
@@ -35,6 +52,7 @@ func main() {
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("dist/static"))))
 	mux.HandleFunc("/", index)
 	mux.HandleFunc("/add", getIngredients)
+	mux.HandleFunc("/freemeek", getMeekTime)
 	handler := cors.Default().Handler(mux)
 	log.Fatal(http.ListenAndServe(":8080", handler))
 }
